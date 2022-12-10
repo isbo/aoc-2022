b40af6d1acbfd1a3847a6f047539a10a1103b47a
Imran Patel <aesboe@gmail.com>
2022-12-09 22:27:17 -0800

    day 10 - squint the answer

---
+++ day10/part12.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	. "aoc-2022/utils"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func getXValues(insts []string) []int {
+	vals := make([]int, 242) // hardcode the max value we'll see
+	var cycle = 1
+	var x = 1
+	vals[cycle] = x
+	for _, inst := range insts {
+		if inst == "noop" {
+			cycle++
+			vals[cycle] = x
+		} else {
+			inc, _ := strconv.Atoi(inst[5:])
+			vals[cycle+1] = x
+			x += inc
+			vals[cycle+2] = x
+			cycle += 2
+		}
+	}
+	return vals
+}
+
+func part1(insts []string) int {
+	vals := getXValues(insts)
+	var ss = 0
+	for _, c := range []int{20, 60, 100, 140, 180, 220} {
+		ss += c * vals[c+1]
+	}
+	return ss
+}
+
+func part2(insts []string) {
+	vals := getXValues(insts)
+	fmt.Println(vals)
+	var cycle = 1
+	for r := 0; r < 6; r++ {
+		for p := 0; p < 40; p++ {
+			x := vals[cycle]
+			if p >= x-1 && p <= x+1 {
+				fmt.Print("#")
+			} else {
+				fmt.Print(".")
+			}
+			cycle++
+		}
+		fmt.Println()
+	}
+}
+
+func main() {
+	file, e := os.Open("input")
+	PanicOnError(e)
+
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	fmt.Println("part1", part1(lines))
+	part2(lines)
+}
